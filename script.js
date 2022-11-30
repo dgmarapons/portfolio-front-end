cdef6afffe82e6834afb7cd4285533b121afbb93
maraninesdg <mara.ponss@gmail.com>
2022-11-30 12:18:13 -0300

    commit 5

--- script.js
+++
@@ -1,3 +0,0 @@
-fetch('http://127.0.0.1:5500/libros.json').then((response) => response.json()).then((libros) => {
-  const nombreDeLibros = libros. map
-  ((libro) => libro.nombre); console.log(nombreDeLibros);});
